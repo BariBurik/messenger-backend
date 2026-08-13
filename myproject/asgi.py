424bb21fd9835ed0aa8177858ae967cee2f638d9
Oleg122 <joeburen122@gmail.com>
2026-08-13 21:35:14 +0300

    asgi repare

--- myproject/asgi.py
+++ myproject/asgi.py
@@ -1,20 +1,30 @@
 import os
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "myproject.settings")
+
 from django.core.asgi import get_asgi_application
+
+# Сначала полностью инициализируем Django.
+django_asgi_app = get_asgi_application()
+
+# Только после этого можно импортировать код,
+# который обращается к Django models.
 from django.urls import path
 from channels.routing import ProtocolTypeRouter, URLRouter
-from strawberry.asgi import GraphQL
 from strawberry.channels import GraphQLWSConsumer
-from strawberry.django.views import AsyncGraphQLView
 
 from messenger.strawberry import schema
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "myproject.settings")
 
 graphql_ws_consumer = GraphQLWSConsumer(schema)
 
 application = ProtocolTypeRouter({
-    "http": get_asgi_application(),
-    "websocket": (URLRouter([
-        path("graphql/subscription/", graphql_ws_consumer.as_asgi(schema=schema)),
-    ])),
+    "http": django_asgi_app,
+
+    "websocket": URLRouter([
+        path(
+            "graphql/subscription/",
+            graphql_ws_consumer.as_asgi(schema=schema)
+        ),
+    ]),
 })
