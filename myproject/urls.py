0862966e04d939e156eec29371b9af3f70cbb1a7
Oleg122 <joeburen122@gmail.com>
2026-08-14 09:11:53 +0300

    repare media

--- myproject/urls.py
+++ myproject/urls.py
@@ -1,9 +1,10 @@
 import logging
 
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, re_path
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.static import serve
 from strawberry.django.views import AsyncGraphQLView
 
 from messenger.CustomGraphQLView import CustomGraphQLView
@@ -21,4 +22,9 @@ urlpatterns = [
     # Strawberry маршруты
     path("graphql/strawberry/", AsyncGraphQLView.as_view(schema=strawberry_schema)),
     path('delete-http-only-cookie/', delete_http_only_cookie, name='delete_http_only_cookie'),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    re_path(
+        r'^media/(?P<path>.*)$',
+        serve,
+        {'document_root': settings.MEDIA_ROOT}
+    ),
+]
